91cd95173b94f7e73a571ee24cb3cede2d544166
Murkhunting <marcmp.arq@gmail.com>
2026-06-11 22:05:19 +0200

    Fix Spline: revert /next to standard import

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -1,9 +1,9 @@
-import Spline from '@splinetool/react-spline/next';
+import Spline from '@splinetool/react-spline';
 
 type SplineSceneProps = {
   scene: string;
 };
 
 export default function SplineScene({ scene }: SplineSceneProps) {
-  return <Spline scene={scene} />;
+  return <Spline scene={scene} style={{ width: '100%', height: '100%' }} />;
 }
