171b585c86e5cd9dc216933c114eca76814d75c9
Murkhunting <marcmp.arq@gmail.com>
2026-06-11 22:04:26 +0200

    Use @splinetool/react-spline/next import

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -1,25 +1,9 @@
-import { useCallback } from 'react';
-import Spline from '@splinetool/react-spline';
-import type { Application } from '@splinetool/runtime';
+import Spline from '@splinetool/react-spline/next';
 
 type SplineSceneProps = {
   scene: string;
 };
 
 export default function SplineScene({ scene }: SplineSceneProps) {
-  const onLoad = useCallback((spline: Application) => {
-    const { renderer, scene: splineScene } = spline;
-    renderer.setClearColor(0x000000, 0);
-    splineScene.background = null;
-    const canvas = renderer.domElement;
-    canvas.style.background = 'transparent';
-  }, []);
-
-  return (
-    <Spline
-      scene={scene}
-      style={{ width: '100%', height: '100%', background: 'transparent' }}
-      onLoad={onLoad}
-    />
-  );
+  return <Spline scene={scene} />;
 }
